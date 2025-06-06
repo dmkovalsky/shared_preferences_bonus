1280252b384957bb34f4c6317548408b3e5c4939
dmkovalsky <dmitrykovalsky@gmail.com>
2025-06-06 11:44:11 +0200

    add shared_preferences dependancy + first UI

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,31 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(title: Text('Shared Preferences Bonus Task')),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Center(
+              child: Column(
+                spacing: 16,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Benutzername',
+                      hintText: 'Geben Sie Ihren Benutzernamen ein',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  FilledButton(
+                    onPressed: () {},
+                    child: Text('Speichern'),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
